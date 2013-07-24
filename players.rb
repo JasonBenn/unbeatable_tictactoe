ed0b83298d1b350a5b893d2ac545e84d408de5c4
Jason Benn <jasoncbenn@gmail.com>
2013-07-24 11:26:57 -0500

    AI detects winning move, if present.

--- players.rb
+++ players.rb
@@ -1,16 +1,19 @@
 require './tictactoe'
 
 class AI
-	def self.next_move(board_string)
+	def self.winning_move(board_string)
 		9.times do |n|
-			dup_string = board_string.split('')[n] = 'X'
-			dup_game = TicTacToe.new(dup_string)
-			return n if dup_game.winner?
+			dup_board = board_string.dup
+			dup_board[n] = 'X'
+			return n if TicTacToe.new(dup_board).winner?
 		end
+		false
 	end
 end
 
 test(
-	AI.next_move('-XX------') == 0,
+	AI.winning_move('-XX------') == 0,
+ 	AI.winning_move('--X-X----') == 6,
+ 	AI.winning_move('--X------') == false
 	)
 puts 'passed!'
