356e51a08ff4fe3b084feb9b581b079e11857747
Jason Benn <jasoncbenn@gmail.com>
2013-07-28 20:03:56 -0500

    AI imagines all possible outcomes of a game.

--- ai.rb
+++ ai.rb
@@ -10,40 +10,35 @@ class AI
 		@icon = icon
 	end
 
-	def winning_move(board_string)
-		empty_spaces(board_string).each do |space|
-			dup_board = board_string.dup
-			dup_board[space] = @icon
-			return space if TicTacToe.new(dup_board).winner?
-		end
-		false
-	end
-
 	def choose_next_move(board_string)
 		return winning_move(board_string) if winning_move(board_string)
 		probabilities_array = calculate_probability(board_string)
 		index_of_max(probabilities_array)
 	end
 
-	def compute_possibilities(board_string, current_player, index, answers = [])
-		game = TicTacToe.new(board_string)
-		puts "board: #{game.board}"
-		winner = winning_move(board_string)
-		return winner if winner
-		puts "winner: #{winner}"
-		puts "answers: #{answers.flatten}"
-		board_string[index] = current_player
-		puts "board_string: #{board_string}"
+	def winning_move(board_string)
 		empty_spaces(board_string).each do |space|
-			answers << compute_possibilities(board_string, current_player, space, answers)
+			dup_board = board_string.dup
+			dup_board[space] = @icon
+			return space if TicTacToe.new(dup_board).winner?
 		end
-		answers
+		nil
 	end
 
 	def calculate_probability(board_string)
 		9.times.map do |space|
 			board_string[space] == '-' ? 1 : 0
 		end
+	end
+
+	def compute_possibilities(board_string, current_player, index, answers = [])
+		board_string[index] = current_player
+		game = TicTacToe.new(board_string)
+		return answers << game.winner? if game.winner?
+		empty_spaces(board_string).each do |space|
+			compute_possibilities(board_string.dup, ['X', 'O'].reject { |icon| icon == current_player }[0], space, answers)
+		end
+		answers
 	end
 
 	def empty_spaces(board_string)
@@ -68,13 +63,15 @@ if $0 == __FILE__ || $0 == 'test.rb'
 	Tester.test(
 		ai.winning_move('-XX------') == 0,
 		ai.winning_move('--X-X----') == 6,
-		ai.winning_move('--X------') == false,
+		ai.winning_move('--X------') == nil,
 		ai.empty_spaces('-XX--XX--') == [0, 3, 4, 7, 8],
 		ai.empty_spaces('---------') == (0..8).to_a,
-		ai.calculate_probability('XXXXXX---') == [0, 0, 0, 0, 0, 0, 1, 1, 1],
 		ai.choose_next_move('XXXXXXXX-') == 8,
 		ai.choose_next_move('-XXXXXXXX') == 0,
 		ai.choose_next_move('-XX-XXXXX') == 0,
+		ai.compute_possibilities('XX-OO-XO-', 'X', 2) == ['X'],
+		ai.compute_possibilities('XX-OO-XO-', 'X', 5).sort == ['D', 'X'],
+		ai.compute_possibilities('XX-OO-XO-', 'X', 8).sort == ['D', 'O']
 		)
 	puts 'passed: ai.rb!'
 end
