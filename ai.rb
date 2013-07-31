29036b376b1c1603c0f7c7898f95e9169c6fd777
Jason Benn <jasoncbenn@gmail.com>
2013-07-31 02:50:57 -0500

    Implement protection against lone danger case.

--- ai.rb
+++ ai.rb
@@ -18,9 +18,23 @@ class AI < Struct.new(:symbol)
 		space_scores = game.empty_spaces.map do |space|
 			[space, game.score(space)]
 		end
+
+		return 1 if danger_case?(game)
+
 		cell = space_scores.max_by { |index, score| score }[0]
 		orig_game.history << [symbol, cell]
 		cell
+	end
+
+	def danger_case?(game)
+		[ game.empty_spaces.count == 6,
+			game.board[4] == symbol,
+			opponent_has_opposite_corners(game.board)
+			].all?
+	end
+
+	def opponent_has_opposite_corners(s)
+		s[2] == other_symbol && s[6] == other_symbol || s[0] == other_symbol && s[8] == other_symbol
 	end
 
 	def other_symbol
@@ -32,14 +46,17 @@ if $0 == __FILE__
 	require './test'
 	ai = AI.new('X')
 
-puts ai.choose_next_move(TicTacToe.new('X-OOO-X-X'))
-
-	# Tester.test(
-	# 	ai.choose_next_move('XX-------') == 2,
-	# 	ai.choose_next_move('OO-------') == 2,
-	# 	ai.choose_next_move('OO-XX----') == 5,
-	# 	ai.choose_next_move('XOX------') == 4,
-	# 	)
+	Tester.test(
+		ai.choose_next_move('XX-------') == 2,
+		ai.choose_next_move('OO-------') == 2,
+		ai.choose_next_move('OO-XX----') == 5,
+		ai.choose_next_move('XOX------') == 4,
+		)
 end
 
 # If opponent's first move is a corner: you can NOT go in the middle.
+# Guard against this case:
+# 	Opp moves first: corner
+# 	We move second: center
+# 	They move third: opposite corner!
+# 	DANGER CASE!  Don't choose other two corners!!
