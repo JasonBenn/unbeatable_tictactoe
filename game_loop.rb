40eaf037b2090230f0428911ab73550467dae00c
Jason Benn <jasoncbenn@gmail.com>
2013-08-26 16:15:55 -0700

    Improve distinction between game and board. DRY

--- game_loop.rb
+++ game_loop.rb
@@ -8,10 +8,11 @@ require './app/helpers/move_validator'
 
 class Game
   include MoveValidator
-  attr_accessor :game, :view, :player1, :player2
+  attr_accessor :game, :board, :view, :player1, :player2
 
   def initialize(args = {})
     @game    = TicTacToe.new
+    @board   = @game.board
     @view    = args[:view]     || View
     @player1 = (args[:player1] || Human).new('X')
     @player2 = (args[:player2] || AI).   new('O')
@@ -19,18 +20,18 @@ class Game
 
   def play
     winner = nil
-    view.show(game.board)
+    view.show(board)
     [player1, player2].cycle do |player|
       break if winner = game.winner?
 
-      cell = player.choose_next_move(game.board)
-      until MoveValidator.move_valid?(game.board, cell)
+      cell = player.choose_next_move(board)
+      until MoveValidator.move_valid?(board, cell)
         view.move_invalid
-        cell = player.choose_next_move(game.board)
+        cell = player.choose_next_move(board)
       end
 
-      game.board[cell] = player.icon
-      view.show(game.board)
+      board[cell] = player.icon
+      view.show(board)
     end
     view.congratulate(winner)
   end
